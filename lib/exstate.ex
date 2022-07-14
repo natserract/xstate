7fac8cb9c85394fea556799809d07eb9c02017cb
natserract <benjaminstwo@gmail.com>
2022-07-14 17:04:00 +0800

    feat: init elixir project

---
+++ lib/exstate.ex
@@ -0,0 +1,18 @@
+defmodule Exstate do
+  @moduledoc """
+  Documentation for `Exstate`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Exstate.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/exstate_test.exs
@@ -0,0 +1,8 @@
+defmodule ExstateTest do
+  use ExUnit.Case
+  doctest Exstate
+
+  test "greets the world" do
+    assert Exstate.hello() == :world
+  end
+end
