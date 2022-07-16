133011a1dcc205d6af9323226458b40db4917f17
natserract <benjaminstwo@gmail.com>
2022-07-16 14:51:18 +0800

    fix: incorrect .credo configuration

--- .credo.exs
+++ .credo.exs
@@ -6,9 +6,11 @@
         included: ["lib/"],
         excluded: []
       },
+      strict: true,
       checks: [
         {Credo.Check.Design.AliasUsage, false},
         {Credo.Check.Readability.AliasOrder, false},
+        {Credo.Check.Warning.IoInspect, priority: :high},
         {Credo.Check.Readability.Specs, []}
       ]
     }
